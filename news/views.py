bfe8cc497b39d2b5aeef9792fececdaf03ab6d17
Juan Manuel Zapata <jmzapatao@eafit.edu.co>
2025-03-03 14:55:50 -0500

    Agregar Requirementstxt

--- news/views.py
+++ news/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import News
 
 def news(request):
-    newszs = News.objects.all().order_by('-date')
-    return render(request, 'news.html', {'newss':newszpys})
+    newszz = News.objects.all().order_by('-date')
+    return render(request, 'news.html', {'newss':newszz})
 
